e5f358f0c058f92989df426e8f4575101c1ccc23
Weydert Michael <michael.weydert@devinci.fr>
2016-01-15 13:00:03 +0100

    Exercise 6 - Rental modification

--- public/index.js
+++ public/index.js
@@ -166,15 +166,16 @@ var rentalModifications = [{
 }];
 
 
+
 //functions to compute price
 function dateToDays(d1,d2)
 {
 	 var date1 = new Date(d1);
 	 var date2 = new Date(d2);
 	 
-	 return result = 1+(date2-date1)/86400000;
+	 return 1+(date2-date1)/86400000;
+	 //return result = 1+(date2-date1)/86400000;
 }
-
 
 function rentPrice(tabDriver, tabCar, tabActors)
 {
@@ -182,6 +183,7 @@ function rentPrice(tabDriver, tabCar, tabActors)
 	{
 		var time = dateToDays(tabDriver[i].pickupDate, tabDriver[i].returnDate);
 		
+		//Calcul des prix, montants pour chaque acteur
 		for(var j=0; j<tabCar.length; j++)
 		{
 			if (tabDriver[i].carId == tabCar[j].id)
@@ -219,6 +221,7 @@ function rentPrice(tabDriver, tabCar, tabActors)
 					deductibleReductionPrice = 4*time;
 				}
 			}
+			
 			var price = priceKm + priceTime + deductibleReductionPrice;
 			
 			var commission = 0.3*(price-deductibleReductionPrice);
@@ -226,7 +229,6 @@ function rentPrice(tabDriver, tabCar, tabActors)
 			var assistance = time*1;
 			var drivy = commission - insurance - assistance + deductibleReductionPrice;
 		}
-		
 		
 		//Modification du tableau des locations
 		tabDriver[i].price = price;
@@ -270,11 +272,138 @@ function rentPrice(tabDriver, tabCar, tabActors)
 }
 
 
+function applyModif(tabDriver, tabCar, tabActors, tabModif)
+{
+	var newPrice = 0;
+	var newCommission = 0;
+	var newInsurance = 0;
+	var newAssistance = 0;
+	var newDrivy = 0;
+	
+	var deltaPrice = 0;
+	var deltaCommission = 0;
+	var deltaInsurance = 0;
+	var deltaAssistance = 0;
+	var deltaDrivy = 0;
+	
+	
+	for(var i=0; i<tabModif.length; i++)
+	{
+		for(var j=0; j<tabDriver.length; j++)
+		{
+			if(tabModif[i].rentalId == tabDriver[j].id)
+			{
+				var dayPrice = 0;
+				var kmPrice = 0;
 
-rentPrice(rentals, cars, actors);
+				//Récupération des prix d'une location
+				for(var r=0; r<tabCar.length; r++)
+				{
+					if(tabDriver[j].carId == tabCar[r].id)
+					{
+						dayPrice = tabCar[r].pricePerDay;
+						kmPrice = tabCar[r].pricePerKm;
+					}
+				}
+				
+				//calcul nouvelle durée
+				var newTime = 0;
+				if(tabModif[i].returnDate) 
+				{
+					if(tabModif[i].pickupDate)
+					{
+						newTime = dateToDays(tabModif[i].pickupDate, tabModif[i].returnDate);
+					}
+					else
+					{
+						newTime = dateToDays(tabDriver[j].pickupDate, tabModif[i].returnDate);
+					}
+				}
+				else
+				{
+					if(tabModif[i].pickupDate)
+					{
+						newTime = dateToDays(tabModif[i].pickupDate, tabDriver[j].returnDate);
+					}
+					else
+					{
+						newTime = dateToDays(tabDriver[j].pickupDate, tabDriver[j].returnDate);
+					}
+				}
+				
+				//Calcul prix nouvelle durée
+				var newPriceTime = 0;
+				for(var k=1; k<=newTime; k++)
+				{
+					if(k==1)
+					{
+						newPriceTime += dayPrice;
+					}
+					else if(1<k && k<5)
+					{
+						newPriceTime += 0.9*dayPrice;
+					}
+					else if(4<k && k<11)
+					{
+						newPriceTime += 0.7*dayPrice;
+					}
+					else if(k>10)
+					{
+						newPriceTime += 0.5*	;
+					}
+				}
+				
+				//calcul nouveau prix distance
+				var newPriceKm = 0;
+				if(tabModif[i].distance)
+				{
+					newPriceKm = tabModif[i].distance*kmPrice;
+				}
+				
+				//Calcul nouveau prix option deductible
+				var newDeductibleReductionPrice = 0;
+				if(tabDriver[i].options.deductibleReduction == true)
+				{
+					newDeductibleReductionPrice = 4*newTime;
+				}
+				
+				//Calculs des nouveaux prix
+				newPrice = newPriceKm + newPriceTime + newDeductibleReductionPrice;
+				newCommission = 0.3*(newPrice-newDeductibleReductionPrice);
+				newInsurance = 0.5*newCommission;
+				newAssistance = newTime*1;
+				newDrivy = newCommission - newInsurance - newAssistance + newDeductibleReductionPrice;
+			}
+			
+			for(var k=0; k<tabActors.length; k++)
+			{
+				if(tabActors[k].rentalId == tabModif[i].rentalId)
+				{
+					//Delta prix
+					deltaPrice = newPrice - tabActors[k].payment[0].amount;
+					deltaCommission = newCommission - tabActors[k].payment[1].amount; 
+					deltaInsurance = newInsurance - tabActors[k].payment[2].amount;
+					deltaAssistance = newAssistance -  tabActors[k].payment[3].amount;
+					deltaDrivy = newDrivy -  tabActors[k].payment[4].amount;
+					
+					//Modification tableau acteurs (reste à du/à payer pour chaque acteur)
+					tabActors[k].payment[0].amount = deltaPrice;
+					tabActors[k].payment[1].amount = deltaCommission;
+					tabActors[k].payment[2].amount = deltaInsurance;
+					tabActors[k].payment[3].amount = deltaAssistance;
+					tabActors[k].payment[4].amount = deltaDrivy;
+				}
+			}
+		}
+	}
+}
+
+rentPrice(rentals, cars, actors, rentalModifications);
+applyModif(rentals, cars, actors, rentalModifications);
 
 console.log(cars);
 console.log(rentals);
+console.log("Les valeurs contenues dans actors sont les montants restant du/à payer après modification");
 console.log(actors);
 console.log(rentalModifications);
 
